de5b4c77cf93fc6e3eaae5a083e495f9f4182104
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-04-18 18:31:37 -0300

    request

--- 8-primeiro-servidor-c-paginas-de-node/index.js
+++ 8-primeiro-servidor-c-paginas-de-node/index.js
@@ -26,4 +26,4 @@ const app = http.createServer((req, res) => {
 
 app.listen(3000, ()=>console.log('Servidor ON na porta 3000!'));
 
-
+ 
---
+++ 9-request/index.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+function handleFile(req, res, callback) {
+   let path = url.parse(req.url, true).pathname;
+
+   if (path == "" || path == "/") {
+      path = "/index.html";
+   }
+   let fileName = "." + path;
+
+   fs.readFile(fileName, (err, data) => {
+      if (err) {
+
+        console.log(!callback);
+         if (callback) {
+            if(!callback(req, res)){
+                res.writeHead(404, { "Content-Type": "text/html;charset=utf-8" });
+                res.end("<h1>Página não encontrada</h1>");
+            };
+         }
+      } else {
+         res.writeHead(200, { "Content-Type": "text/html" });
+         res.write(data);
+         res.end();
+      }
+   });
+}
+
+function handleRequest(req, res) {
+   let path = url.parse(req.url, true).pathname;
+
+   if (path == "/teste") {
+      res.end("teste");
+      return true;
+   }
+   return false;
+}
+
+const app = http.createServer((req, res) => {
+   handleFile(req, res, handleRequest);
+});
+
+app.listen(3000, () => console.log("Servidor ON na porta 3000!"));
+
